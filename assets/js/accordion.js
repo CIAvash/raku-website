98c90162b180106f8bce4b35aba391f498051e8b
CIAvash <CIAvash@users.noreply.github.com>
2021-06-19 14:28:37 +0000

    deploy: 975f7cacce26f569209a981059706f9588643d99

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,25 +1,28 @@
-let accordionBtns = document.querySelectorAll('.accordion-button');
+let accordionBtns = document.querySelectorAll('.raku-accordion-input');
 
-accordionBtns.forEach(element => element.addEventListener('click', function(event) {
-    let element = this;
-    let accordion = document.querySelector('.accordion');
+accordionBtns.forEach(element => ['click', 'change'].forEach(function(event) {
+    element.addEventListener(event, function() {
+        let element = this;
+        let accordion = document.querySelector('.raku-accordion-container');
 
-    if (document.documentElement.scrollTop > accordion.parentElement.offsetTop) {
+        if (document.documentElement.scrollTop > accordion.parentElement.offsetTop) {
+            setTimeout(() =>
+                document.documentElement.scrollTo({
+                    left: 0,
+                    top: accordion.parentElement.offsetTop,
+                    behavior: 'smooth'
+                }),
+                100);
+        }
+
+        // Add delay for small screens
         setTimeout(() =>
-            document.documentElement.scrollTo({
+            accordion.scrollTo({
                 left: 0,
-                top: accordion.parentElement.offsetTop,
+                top: element.offsetTop - 25,
                 behavior: 'smooth'
             }),
-            100);
-    }
-
-    // Add delay for small screens
-    setTimeout(() =>
-        accordion.scrollTo({
-            left: 0,
-            top: element.offsetTop - 10,
-            behavior: 'smooth'
-        }),
-        100);
+            100
+        );
+    });
 }));
